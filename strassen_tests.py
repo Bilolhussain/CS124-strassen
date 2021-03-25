b397d14250f6b8c85681754fb83f7bb53f8b5ecb
Bilal Hussain <bilal_hussain@college.harvard.edu>
2021-03-25 00:42:03 -0400

    Update strassen_tests.py

--- strassen_tests.py
+++ strassen_tests.py
@@ -1,30 +1,28 @@
-    from datetime import datetime
-    import sys
-    from random import randint
-    from strassen import *
+import datetime
+import sys
+import random
+import math
+from strassen import *
 
-    d = int(sys.arv[2])
-    t = [15, 20, 25, 35, 45, 60, 90, 100]
-    original_trials = 1
-    trials = original_trials
-    begin = datetime.datetime.now()
+dim = int(sys.arv[2])
+    
+m1 = populate_matrix(generate_matrix(dim))
+m2 = populate_matrix(generate_matrix(dim))
+    
+def ceil(n):
+  return int(-1 * n // 1 * -1)    
 
-    while (trials > 0):
-        m1 = populate_matrix(generate_matrix(d))
-        m2 = populate_matrix(generate_matrix(d))
-        print("MATRIX 1")
-        printmatrix(m1)
-        print("MATRIX 2")
-        printmatrix(m2)
-        product = strassen(m1, m2, t)
-        print("MATRIX PRODUCT " + str(trials) + ":")
-        printmatrix(product)
-        trials -= 1
-
-    end = datetime.datetime.now()
-    time = end - begin
-    print("dimension: " + str(d))
-    print("threshold: " + str(t))
-    print("trials: " + str(original_trials))
-    print("total time: " + str(time))
-    print("time per trial: " + str(time/original_trials))   
+t = [ceil(dim/2**n) for n in range(1, ceil(math.log2(dim)))]  
+for x in t:
+   t_start = datetime.datetime.now()
+   product = strassen(m1, m2, x)
+   end = datetime.datetime.now()
+   time = end - begin
+   print("Threshold:")
+   print(x)
+   print("Time taken", str(time))
+   #printmatrix(product)         
+    
+   
+    
+        
